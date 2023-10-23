bd94b7a880078bbc7a79539d7b775e205f7ad11e
Sstanley45 <stanleynjuguna445@gmail.com>
2023-10-23 20:59:58 +0300

    create-prompt front-end

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -8,15 +8,39 @@ import Form from "@/components/Form";
 
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
     tag: '',
   });
-  
+
 
   const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag : post.tag,
+        })
+      }) 
+
+      if (response.ok) {
+        router.push('/');
+      }
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   }
 
 
